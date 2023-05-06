44b77a323a8af0631dc57ec1328c9559a1e491d9
devstygian <hackstygian@gmail.com>
2023-05-06 14:15:47 +0800

    Upadate 050623
    Lets try this again! I added some new feature

--- pages/scrip2.js
+++ pages/scrip2.js
@@ -8,22 +8,6 @@ if (stockNumber > 0) {
     stockElement.textContent = 'Out of stock';
 }
 
-// set the stock number dynamically
-var stockNumber = 50; // replace with your actual stock number
-var stockElement = document.querySelector('.stock1');
-if (stockNumber > 0) {
-    stockElement.textContent = 'In stock: ' + stockNumber;
-} else {
-    stockElement.classList.add('out-of-stock');
-    stockElement.textContent = 'Out of stock';
-}
 
-// set the stock number dynamically
-var stockNumber = 50; // replace with your actual stock number
-var stockElement = document.querySelector('.stock2');
-if (stockNumber > 0) {
-    stockElement.textContent = 'In stock: ' + stockNumber;
-} else {
-    stockElement.classList.add('out-of-stock');
-    stockElement.textContent = 'Out of stock';
-}
+
+
